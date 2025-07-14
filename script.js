fec491149b40fa37af94891397647c74607bd557
jinzoon <186304379+jinzoon@users.noreply.github.com>
2025-07-15 08:16:02 +0900

    Write basic pseudocode

---
+++ script.js
@@ -0,0 +1,17 @@
+// this  program is going to 
+// play rock paper scissors
+
+//first it will get the computer's
+// choice
+// it will use a random number to do this
+// 0 rock, 1 paper, 2, scissors
+
+// it will then take the players choice
+
+//these two will be checked against each other
+
+// the winner score will be increased and it will output 
+// the winner  with the current score 
+
+//the game will run four more times
+// and then the final winner will be announced
